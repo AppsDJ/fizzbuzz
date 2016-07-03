54eab490eba2eb667b4d815a3a25f78126414dd2
6eff <6eff@mail.ru>
2016-07-03 17:23:37 +0300

    rspec test for multiple of 3

--- spec/fizzbuz_spec.rb
+++ spec/fizzbuz_spec.rb
@@ -1,9 +1,9 @@
 require "fizzbuzz"
 
 describe 'fizzbuzz' do
-  number = [3,6,9,12]
-  it 'returns "fizz" if given multipe of 3'
-    number.each do |num|
-      expect(fizzbuzz(num)).to eq 'fizz'   end
-  end
+  it 'returns "fizz" if given multipe of 3' do
+    [3,6,9,12].each do |num|
+      expect(fizzbuzz(num)).to eq 'fizz'
+    end
+end
 end
