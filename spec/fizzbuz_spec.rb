655900d1bf2fad8c644702e06bacff12893895f8
Titus Iudean <iudeant@gmail.com>
2016-07-03 15:17:31 +0100

    worked on first test

--- spec/fizzbuz_spec.rb
+++ spec/fizzbuz_spec.rb
@@ -1,6 +1,9 @@
-describe 'fizzbuzz' do |number|
+require "fizzbuzz"
+
+describe 'fizzbuzz' do
   number = [3,6,9,12]
   it 'returns "fizz" if given multipe of 3'
-  number.each do exspect(fizzbuz(number)).to eq 'fizz'
+    number.each do |num|
+      expect(fizzbuzz(num)).to eq 'fizz'   end
   end
 end
